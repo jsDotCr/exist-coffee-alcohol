2d210abf784ef98fd7b5ea7ac98a9a05c4411a34
jsdotcr <rocco.c@smart-host.co>
2018-01-05 22:03:20 +0100

    fix(build): clean up scripts and url configuration file by using custom env variable

--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,22 @@
-const apiRoot = 'https://exist.io'
-const clientId = process.env.CLIENT_ID
-const clientUrl = global && global.location
-  ? global.location.origin + global.location.pathname
-  : ''
+const { URL = global.URL } = require('url')
 
-switch (process.env.NODE_ENV) {
-  case 'development':
-    exports.clientRequestTokenUrl = 'http://localhost:8170/jsDotCr/sxe/authorize/'
+const clientId = process.env.CLIENT_ID
+const apiRoot = 'https://exist.io'
+let clientUrl = ''
+
+switch (process.env.SXE_ENV) {
+  case 'local':
+    clientUrl = 'http://localhost:8170/jsDotCr/sxe'
     break
-  case 'production':
-    exports.clientRequestTokenUrl = new global.URL('authorize/', clientUrl)
+  default:
+    clientUrl = `https://functions.lib.id/jsDotCr/sxe@${process.env.SXE_ENV}`
 }
 
-exports.authorizeAppUrl = `${apiRoot}/oauth2/authorize?response_type=code&redirect_uri=${clientUrl}&scope=read+write&client_id=${clientId}`
-exports.accessTokenUrl = `${apiRoot}/oauth2/access_token`
-exports.acquireUrl = `${apiRoot}/api/1/attributes/acquire/`
-exports.trackUrl = `${apiRoot}/api/1/attributes/$SOMETHING/`
+exports.clientRequestTokenUrl = new URL('authorize/', clientUrl)
+exports.clientListOwnedAttributesUrl = new URL('attributes/', clientUrl)
+
+exports.authorizeAppUrl = new URL(`oauth2/authorize?response_type=code&redirect_uri=${clientUrl}&scope=read+write&client_id=${clientId}`, apiRoot)
+exports.accessTokenUrl = new URL(`oauth2/access_token`, apiRoot)
+exports.listOwnedAttributesUrl = new URL(`api/1/attributes/owned/`, apiRoot)
+exports.acquireAttributesUrl = new URL(`api/1/attributes/acquire/`, apiRoot)
+exports.trackUrl = new URL(`api/1/attributes/$SOMETHING/`, apiRoot)
